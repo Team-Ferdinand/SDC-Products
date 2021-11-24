f16181ffa234531c6678c11f3e4ca4c6bd4b92b6
Matthew Seagren <mattseagren@gmail.com>
2021-11-24 10:30:20 -0600

    Saving progress

--- server/database/models/products.js
+++ server/database/models/products.js
@@ -13,3 +13,36 @@ module.exports = {
     return db.pool.query(`SELECT products.id, products.name, products.slogan, products.description, products.category, products.default_price, (SELECT json_agg(json_build_object('features', features.features, 'value', features.value))features FROM features WHERE features.id_products=products.id) FROM products WHERE products.id = ${id} GROUP BY 1 `);
   }
 }
+
+
+
+{
+  "id_products": 3,
+    "results": [
+      {
+        "style_id": 11,
+        "name": "Black",
+        "original_price": 40,
+        "sale_price": "null",
+        "default": "1",
+        "photos": [{}, {}],
+        "skus": {
+              {},
+      {}
+            }
+          },
+{
+  "style_id": 11,
+    "name": "Black",
+      "original_price": 40,
+        "sale_price": "null",
+          "default": "1",
+            "photos": [{}, {}],
+              "skus": {
+    { },
+    { }
+  }
+}
+
+  ]
+}
